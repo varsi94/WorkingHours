b3a5e4e80cbbb2cf8fcfac2ab1e6ab2416f6a907
Ecsedi Gergő <ecsedigergo@gmail.com>
2016-10-26 13:27:42 +0200

    Added TagControl Visibility

---
+++ WorkingHours.Desktop/Converters/RoleToVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using WorkingHours.Shared.Model;
+
+namespace WorkingHours.Desktop.Converters
+{
+    class RoleToVisibilityConverter : IValueConverter
+    {
+        public Visibility ManagerValue { get; set; }
+        public Visibility EmployeeValue { get; set; }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((string)value == Roles.Manager.ToString())
+            {
+                return ManagerValue;
+            }
+            else
+            {
+                return EmployeeValue;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
